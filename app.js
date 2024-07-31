b2936075e6f782bb118f1507cbbf01da563b2dfa
Sumitha2502 <sumitha0996@gmail.com>
2024-07-31 15:15:11 +0530

    user endpoints defined --readme.md

---
+++ app.js
@@ -0,0 +1,48 @@
+// require express
+const express = require('express');
+
+// import the user routes
+const userRouter = require('./routes/userRoutes');
+
+// create an express app
+const app = express();
+
+// require cors
+const cors = require('cors');
+
+// require cookie-parser
+const cookieParser = require('cookie-parser');
+
+// require morgan
+const morgan = require('morgan');
+const companyRouter = require('./routes/companyRoutes');
+const jobRouter = require('./routes/jobRoutes');
+
+// use the cors middleware
+app.use(cors({
+    origin: '*', // allow all origins
+    credentials: true
+}));
+
+// use the cookie-parser middleware
+app.use(cookieParser());
+
+// use the morgan middleware
+app.use(morgan('dev'));
+
+// use the express json middleware
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.head('/api', (req, res) => {
+    res.send('Welcome to the Job Portal API');
+});
+
+// define the endpoints
+app.use('/api/users', userRouter);
+app.use('/api/companies', companyRouter);
+app.use('/api/jobs', jobRouter);
+
+// export the app module
+module.exports = app;
--- readme.md
+++ readme.md
@@ -17,3 +17,26 @@ Backend Application is a RESTful API developed using Node.js and Express.js. It 
 -[]MongoDB Compass: To visualize the data
 
 
+#API Endpoints
+
+##Users
+
+#Unauthenticated user
+
+POST /api/users - Create a new user
+POST /api/users/login - Login user
+
+#Authenticated user
+
+POST /api/users/logout - Logout user
+GET /api/users/me - Get current user
+PUT /api/users/me - Update current user
+DELETE /api/users/me - Delete current user
+
+#Authenticated - Admin:
+GET /api/users - Get all users
+GET /api/users/:id - Get user by ID
+PUT /api/users/:id - Update user by ID
+DELETE /api/users/:id - Delete user by ID
+
+
