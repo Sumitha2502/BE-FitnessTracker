d135e03ca927598b3a5f6a67eed19985bc54907e
Sumitha2502 <sumitha0996@gmail.com>
2024-08-02 12:16:51 +0530

    server updated

--- models/Goals.js
+++ models/Goals.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(cors());
 
-mongoose.connect('MONGO_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('MONGO_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const goalSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    targetDate: String,
-    // achieved: { type: Boolean, default: false }
-});
+// const goalSchema = new mongoose.Schema({
+//     title: String,
+//     description: String,
+//     targetDate: String,
+//     // achieved: { type: Boolean, default: false }
+// });
 
-const Goal = mongoose.model('Goal', goalSchema);
+// const Goal = mongoose.model('Goal', goalSchema);
 
-app.get('/api/goals', async (req, res) => {
-    const goals = await Goal.find();
-    res.json(goals);
-});
+// app.get('/api/goals', async (req, res) => {
+//     const goals = await Goal.find();
+//     res.json(goals);
+// });
 
-app.post('/api/goals', async (req, res) => {
-    const newGoal = new Goal(req.body);
-    await newGoal.save();
-    res.json(newGoal);
-});
+// app.post('/api/goals', async (req, res) => {
+//     const newGoal = new Goal(req.body);
+//     await newGoal.save();
+//     res.json(newGoal);
+// });
 
-app.put('/api/goals/:id', async (req, res) => {
-    await Goal.findByIdAndUpdate(req.params.id, { achieved: true });
-    res.json({ success: true });
-});
+// app.put('/api/goals/:id', async (req, res) => {
+//     await Goal.findByIdAndUpdate(req.params.id, { achieved: true });
+//     res.json({ success: true });
+// });
 
 
-module.exports = mongoose.model('Goal', goalSchema, 'goal');
+// module.exports = mongoose.model('Goal', goalSchema, 'goal');
